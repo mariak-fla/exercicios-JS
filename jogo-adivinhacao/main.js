9b9ddd791ba2e69fdf0f019fc77d6af4c4ce1225
Maria Almeida <113478264+mariak-fla@users.noreply.github.com>
2023-05-10 13:43:28 -0300

    Update main.js

--- jogo-adivinhacao/main.js
+++ jogo-adivinhacao/main.js
@@ -1,32 +1,48 @@
+// variáveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
-
-const randomNumber = Math.round(Math.random() * 10)
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-// callback
+
+//eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', enterReset)
+
+
+// funções callback
 function handleTryClick(e) {
     e.preventDefault();
     const inputNumber = document.getElementById("inputNumber").value;
 
     if(Number(inputNumber) == randomNumber) {
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+        toggleScreen()
 
         document.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativa(s)!`
+    } else if (0 > Number(inputNumber) || Number(inputNumber) > 10) {
+        alert("Não é possível chutar este número!")
     }
     
     inputNumber.value = ""
     xAttempts++
 }
 
-// eventos
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
+function handleResetClick() {
+    toggleScreen()
     xAttempts = 1
-})
+    randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
+
+function enterReset(event) {
+    if(event.key == 'Enter' && screen1.classList.contains("hide")) {
+        handleResetClick()
+    }
+}
