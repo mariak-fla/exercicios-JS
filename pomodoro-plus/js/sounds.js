9e26697a41e747aa5498d31428f7833b2fe7f6da
Maria Almeida <113478264+mariak-fla@users.noreply.github.com>
2023-05-30 10:49:50 -0300

    replace audio files url

--- pomodoro-plus/js/sounds.js
+++ pomodoro-plus/js/sounds.js
@@ -1,21 +1,26 @@
 export default function() {
     const btnPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
+
     const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
-    const forestBgAudio = new Audio("https://github.com/mariak-fla/exercicios-JS/blob/main/pomodoro-plus/js/bgAudio/Floresta.wav?raw=true");
+
+    const forestBgAudio = new Audio("https://drive.google.com/uc?id=1CRHkV72WUMdcqec5GT_KdsqFz0z3VAOA")
     forestBgAudio.loop = true
-    const rainBgAudio = new Audio("https://github.com/mariak-fla/exercicios-JS/blob/main/pomodoro-plus/js/bgAudio/Chuva.wav?raw=true");
+
+    const rainBgAudio = new Audio("https://drive.google.com/uc?id=1Ip8xBqAUJ-bty51Wz8JBtX_bWXCgA0P2")
     rainBgAudio.loop = true
-    const cafeBgAudio = new Audio("https://github.com/mariak-fla/exercicios-JS/blob/main/pomodoro-plus/js/bgAudio/Cafeteria.wav?raw=true");
+
+    const cafeBgAudio = new Audio("https://drive.google.com/uc?id=1OxLKpCwg2wrxXFNUHgZxJ51QEt0ac5RA")
     cafeBgAudio.loop = true
-    const fireBgAudio = new Audio("https://github.com/mariak-fla/exercicios-JS/blob/main/pomodoro-plus/js/bgAudio/Lareira.wav?raw=true");
+
+    const fireBgAudio = new Audio("https://drive.google.com/uc?id=1MakaBPxJvTa_whaSM3kEbRcxiVd1GRCB")
     fireBgAudio.loop = true
-    
+
     let isPlaying = {
         forest: false,
         rain: false,
         fire: false,
         cafe: false
-    };
+    }
     
     function pressBtn() {
         btnPressAudio.play()
